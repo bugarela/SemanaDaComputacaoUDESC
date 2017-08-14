de81dbdf4155310f8ea2be9d526aee463d6fdc19
Gabriela Moreira <gabrielamoreira05@gmail.com>
2017-08-13 22:27:59 -0400

    first

---
+++ edicao2016/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Semana da Computa&ccedil;&atilde;o</title>
+<meta name="author" content="SCXVIII">
+<meta name="date" content="2016-09-19T20:41:43-0300">
+<meta name="copyright" content="">
+<meta name="keywords" content="">
+<meta name="description" content="P&aacute;gina da Semana da Computa&ccedil;&atilde;o da UDESC">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<script type="text/javascript" src="script.js"></script>
+<style type="text/css">
+   @import url(estiloHTML.css);
+</style>
+<style type="text/css">
+   @import url(estilo.css);
+</style>
+<!-- estilo para internet explorer com coment&aacute;rio condicional -->
+<!--[if lte IE 7]>
+	<link rel="stylesheet" href="estilo.css" type="text/css" />
+<![endif]-->
+</head>
+<body>
+
+<div id="content">
+
+	<div align="center">
+		<div id="header_new">
+				<a href="index.html"><img src="imagens/SDCC2016/cabecalho.jpg" width="776" height="220" border="0" alt=""></a> 
+		</div>
+		<div id="menu_new">
+			<ul id="menu_inner_new">
+				<li><a class="link7_new" href="index.html" onfocus="blur()">Home</a></li>
+				<li><a class="link7_new" target="_blank" href="http://www.joinville.udesc.br/portal/siac/lista_atividades.php?letra=DCC" onfocus="blur()">Inscri&ccedil;&otilde;es</a></li>
+				<li><a class="link7_new" href="imagens/SDCC2016/programacaoSCXVIII_v2.pdf"> Programa&ccedil;&atilde;o</a></li>				
+				<li><a class="link7_new" target="_blank" href="https://drive.google.com/drive/folders/0B_SFOhQ4yBl6TDF5TWI3Q0tQXzA" onfocus="blur()">Certificados</a></li>
+				<li><a class="link7_new" href="imagens/SDCC2016/XVIIISAC2016.pdf"> Estatisticas </a></li>
+ 			</ul>
+		</div>
+	</div>
+
+  <div id="page">
+
+    <div id="columns" class="widthPad">
+
+    	<div class="floatLeft width70">
+    		
+			
+				A Semana da Computa&ccedil;&atilde;o &eacute; um evento anual desenvolvido no Departamento de Ci&ecircncia da Computa&ccedil;&atilde;o do 
+				Centro de Ci&ecirc;ncias Tecnol&oacute;gicas da Universidade do Estado de Santa Catarina. O principal objetivo da Semana da Computa&ccedil;&atilde;o 
+				&eacute; difundir conhecimentos estabelecendo uma integra&ccedil;&atilde;o entre professores, alunos e comunidade 
+				t&eacutecnico-cient&iacutefica, por meio de palestras, minicursos e atividades culturais. <p>
+
+				Endere&ccedil;o: Campus Universit&aacute;rio Prof. Avelino Marcante s/n. Bairro Bom Retiro. Joinville, SC, Brasil. CEP 89223-100. 
+				</br><br>
+					
+				Os locais espec&iacuteficos (salas/laborat&oacuterios) das palestras e minicursos devem ser verificados na programa&ccedil;&atilde;o. <p>
+
+				E-MAIL para contato: <a href="mailto:semanadacomputacaocct@gmail.com"> Aqui</a><br>
+				
+				
+			  	Link para o site no Facebook: <a href="https://www.facebook.com/SemanaDaComputacaoUDESC"> Aqui </a><br/>
+			  	
+				Link para o CCT/UDESC: <a href="http://www.joinville.udesc.br/"> Aqui</a>
+				</br>
+			
+		</div>
+    	
+
+  </div>
+
+
+</div>
+
+<div id="footer">
+  <div id="bg" class="width">
+  		<br />
+		<div class="floatLeft width33">
+		<img src="imagens/SDCC2016/realizacaoPatrocinioSDCC2016_mini_1.gif" border="0" alt="">
+  		<div/>
+
+  </div>
+</div>
+</body>
+</html>
+    
